24486b373beff96f5c8666a1db999e51d8a815ee
garg-madhur-garg <madhur.gargjls@gmail.com>
2026-08-16 20:16:31 +0530

    Progressive app with SB

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'hindi-vedabase-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './css/main.css',
+  './js/db.js',
+  './js/search.js',
+  './js/sb-chapters-data.js',
+  './js/seed-data.js',
+  './js/app.js',
+  './vedabase.ico',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200 && event.request.method === 'GET') {
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return networkResponse;
+      }).catch(() => {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
